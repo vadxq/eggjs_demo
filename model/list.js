25d63ec0ba85c53fc049842162f8901664646503
vadxq <dongxianlin@vadxq.com>
2019-07-17 18:04:34 +0800

    Add:fix something

--- model/list.js
+++ model/list.js
@@ -5,12 +5,9 @@ module.exports = app => {
   const Schema = mongoose.Schema;
 
   const ListSchema = new Schema({
-    _id: { type: Schema.Types.ObjectId },
     title: { type: String },
     img: { type: String },
     content: { type: String },
-  }, {
-    versionKey: false,
   });
 
   return mongoose.model('List', ListSchema);
